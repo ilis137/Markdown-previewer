eeafc701537a919d164304c30902aa736bf1c5ee
ilis137 <32433239+ilis137@users.noreply.github.com>
2018-10-20 06:38:01 +0530

    markdown added

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,39 @@ import FormGroup from "react-bootstrap/lib/FormGroup";
 import ControlLabel from "react-bootstrap/lib/ControlLabel";
 import "./App.css";
 
+let marked = require("marked");
+
 class App extends Component {
+  state = {
+    markdown: ""
+  };
+
+  handleChange = e => {
+    this.setState({
+      markdown: e.target.value
+    });
+    console.log(this.state.markdown);
+  };
+
   render() {
     return (
       <div className="App">
         <FormGroup className="form" controlId="formControlTextArea">
-          <ControlLabel>Enter Markdown here...</ControlLabel>
+          <ControlLabel>Editor</ControlLabel>
           <FormControl
             className="markdownText"
             componentClass="textarea"
             placeholder="enter markdown"
+            onChange={this.handleChange}
           />
         </FormGroup>
 
         <FormGroup className="preview" controlId="formControlTextArea">
-          <ControlLabel>Preview</ControlLabel>
-          <FormControl className="previewText" componentClass="textarea" />
+          <h1>Previewer</h1>
+          <div
+            className="preview-box"
+            dangerouslySetInnerHTML={{ __html: marked(this.state.markdown) }}
+          />
         </FormGroup>
       </div>
     );
